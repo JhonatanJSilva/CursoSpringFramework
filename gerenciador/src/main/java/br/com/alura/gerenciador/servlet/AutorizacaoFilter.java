c22bb439da4cd560bcbb6bb15643102d4eef0caa
Jhonatan Silva <jhonatan.silva@ibratan.com.br>
2022-06-13 17:21:43 -0300

    ESTUDOS segunda feria 2

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +17,12 @@ import javax.servlet.http.HttpSession;
 //@WebFilter(urlPatterns="/*")
 public class AutorizacaoFilter extends HttpFilter implements Filter {
        
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 	
 		System.out.println("AutorizacaoFilter");
